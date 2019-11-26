4803058876d085f85421f1e18a249892b3df023b
김태형 <taehyeong224@gmail.com>
2019-11-26 14:57:57 +0900

    add post route

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,20 @@
 import {RouterInterface} from "./util/interface";
 import {Method} from "./util/enum";
 import {ShortenerController} from "./controller/ShortenerController";
-import {checkContentType} from "./middleware/contentType";
+import {ShortenerServiceImpl} from "./service/ShortenerServiceImpl";
 
-const shortenerController = new ShortenerController();
+const shortenerController = new ShortenerController(new ShortenerServiceImpl());
 
 export const AppRoutes: RouterInterface[] = [
     {
         path: "/:id",
         method: Method.GET,
         action: shortenerController.getOne
+    },
+    {
+        path: "/register.json",
+        method: Method.POST,
+        action: shortenerController.create
     }
 ];
 
