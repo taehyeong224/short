a7065bf7885af4c629cbfc4bd55fd7a5cc29c77a
김태형 <taehyeong224@gmail.com>
2019-11-26 14:57:30 +0900

    add shortenerService dependency change function to arrow funtion

--- src/controller/ShortenerController.ts
+++ src/controller/ShortenerController.ts
@@ -1,21 +1,38 @@
 import {Request, Response} from "express";
 import {BaseController} from "./base/BaseController";
+import {checkValidation, createURIValid} from "../util/joi";
+import {ShortenerService} from "../service/ShortenerService";
+import {UrlAndDuplicated} from "../config/Type";
+import {ErrorHandle} from "../util/ErrorHandle";
+import {Log} from "../util/Log";
 
 export class ShortenerController extends BaseController {
-    constructor() {
+    private shortenerService: ShortenerService;
+
+    constructor(shortenerService: ShortenerService) {
         super();
+        this.shortenerService = shortenerService;
     }
 
-    create(req: Request, res: Response): void {
-    }
+    public create = (req: Request, res: Response): void => {
+        try {
+            checkValidation(req.query, createURIValid);
+            const {url} = req.query;
+            const result: UrlAndDuplicated = this.shortenerService.generate(url);
+            res.status(result.duplicated ? 200 : 201).json({url: result.url});
+        } catch (e) {
+            Log.error("ShortenerController > error : ", e);
+            ErrorHandle(req, res, e);
+        }
+    };
 
-    delete(req: Request, res: Response): void {
-    }
+    public delete = (req: Request, res: Response): void => {
+    };
 
-    getOne(req: Request, res: Response): void {
+    public getOne = (req: Request, res: Response): void => {
         res.status(200).json({msg: "hello"})
-    }
+    };
 
-    update(req: Request, res: Response): void {
+    public update = (req: Request, res: Response): void => {
     }
 }
