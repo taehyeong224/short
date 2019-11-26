ae79cc0708fc38f35dc238606a35746105a3d672
김태형 <taehyeong224@gmail.com>
2019-11-26 16:42:37 +0900

    add getByShorten

--- src/service/ShortenerStatService.ts
+++ src/service/ShortenerStatService.ts
@@ -1,6 +1,8 @@
 import {Shorten} from "../entity/Shorten";
 import {ShortenStat} from "../entity/ShortenStat";
+import {Stat} from "../config/Type";
 
 export interface ShortenerStatService {
     create(shorten: Shorten): Promise<ShortenStat>;
+    getByShorten(shorten: Shorten): Promise<Stat[]>
 }
--- src/service/ShortenerStatServiceImpl.ts
+++ src/service/ShortenerStatServiceImpl.ts
@@ -1,10 +1,20 @@
 import {ShortenerStatService} from "./ShortenerStatService";
 import {Shorten} from "../entity/Shorten";
 import {ShortenStat} from "../entity/ShortenStat";
+import {Stat} from "../config/Type";
 
 export class ShortenerStatServiceImpl implements ShortenerStatService {
 
     public create = (shorten: Shorten): Promise<ShortenStat> => {
         return ShortenStat.create({shorten: shorten}).save()
     };
+
+    public getByShorten = async (shorten: Shorten): Promise<Stat[]> => {
+        return ShortenStat.query(`
+            SELECT   CONCAT(DATE(visitDate), ' ', HOUR(visitDate), ':00:00') AS 'at',COUNT(*) AS \`visits\`
+            FROM     shorten_stat
+            WHERE    shortenId = '${shorten.id}' AND visitDate BETWEEN '2019-11-25' AND NOW()
+            GROUP BY HOUR(visitDate);
+        `)
+    }
 }
