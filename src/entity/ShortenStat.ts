f18db5acb3e79f19fc41ce93af004c09cbeecbf9
김태형 <taehyeong224@gmail.com>
2019-11-26 16:03:27 +0900

    extends BaseEntity

--- src/entity/ShortenStat.ts
+++ src/entity/ShortenStat.ts
@@ -1,8 +1,8 @@
-import {CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {BaseEntity, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
 import {Shorten} from "./Shorten";
 
 @Entity()
-export class ShortenStat {
+export class ShortenStat extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
